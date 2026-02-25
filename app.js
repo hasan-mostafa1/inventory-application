1b00cb6ea1d3a7feea90bd8abadc220ccd147a06
Hasan Mostafa <hasanalimostafa1@gmail.com>
2026-02-25 15:59:01 +0300

    Setup express app and database configuration

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app - listening on port ${PORT}`);
+});
+
+const viewsPath = path.join(__dirname, "views");
+app.set("views", viewsPath);
+app.set("view engine", "ejs");
+
+//Error middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
